0789966d5b09a02dfae8158c4aa1bea9349b5d1a
Indrajith Indraprastham <mail@indrajith.me>
2024-03-23 23:27:49 +0530

    feat: redirect home to rupx.in

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './components/Home'
 import NotFoundPage from './components/NotFoundPage'
+import Redirect from './components/Redirect'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route exact path="/" element={<Home />} /> */}
+        <Route exact path="/" element={<Redirect to='https://rupx.in' />} />
         <Route exact path="/:qrCode" element={<Home />} />
         <Route path="/404" element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to="/404" />} />
